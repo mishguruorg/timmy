1238a8a0ddaee50d8f402d35c0b5f315d11042cb
George Czabania <george@czabania.com>
2017-01-20 10:45:02 +1300

    Support packages and clean up paths

--- bin/timmy.js
+++ bin/timmy.js
@@ -1,11 +1,29 @@
 #!/usr/bin/env node
 
 const path = require('path')
+const flags = require('flags')
+const resolve = require('resolve')
 
-const modulePath = path.join(process.cwd(), process.argv[2])
+flags.defineString('package', null, 'Name of a package')
+flags.defineString('file', null, 'Path to a file')
+flags.parse()
+
+const packagePath = flags.get('package')
+const filePath = flags.get('file')
+const cwd = process.cwd()
+
+let modulePath = null
+
+if (packagePath != null) {
+  modulePath = resolve.sync(packagePath, {basedir: cwd})
+} else if (filePath != null) {
+  modulePath = path.join(cwd, filePath)
+} else {
+  throw new Error('Must specify --package or --file')
+}
 
 const {default: timmy} = require('../dist')
 
 require(modulePath)
 
-timmy.printStats()
+timmy.printStats(modulePath)
